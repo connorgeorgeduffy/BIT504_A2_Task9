f63ba306562deef6bf943c77d7057675ccba4d2f
Connor Duffy <connor.duffy@teachingcouncil.nz>
2026-08-20 10:10:12 +1200

    Add additional output message

--- src/Default/Task_9_Class.java
+++ src/Default/Task_9_Class.java
@@ -4,8 +4,10 @@ public class Task_9_Class {
 
 	public static void main(String[] args) {
 		System.out.println("BIT504 Assignment 2 Task 9 by Connor Duffy");
-        System.out.println("Python is easier to understand");
+        System.out.println("Python is easier to understand than java");
         System.out.println("OOP is confusing");
+        
+        System.out.println("This is a new text line to commit");
 
 	}
 
